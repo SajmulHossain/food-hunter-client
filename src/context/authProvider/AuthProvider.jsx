4dec60a6e7c307a367a1880413232ef664caaa0d
Sajmul <sajmul1427@gmail.com>
2024-12-22 19:28:24 +0600

    google sign in setup done

--- src/context/authProvider/AuthProvider.jsx
+++ src/context/authProvider/AuthProvider.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import AuthContext from "./AuthContext";
+import auth from "../../firebase/firebase.init";
 
 const AuthProvider = ({children}) => {
 
-  
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = (email, password) => {
+    return signInWithPopup(auth, email, password);
+  }
 
   return (
     <AuthContext.Provider value={'kire'}>{children}</AuthContext.Provider>
